9c8b2ba3e1edcff7f0ca66c63bb92156c5a16e4a
igorushqa <since1925@mail.ru>
2023-07-16 23:12:13 +0700

    add settings commit

--- settings.py
+++ settings.py
@@ -1,8 +1,9 @@
 from pathlib import Path
+
 import json
 
 CURRENT_PATH = Path(__file__).parent
-PATH_WITH_FIXTURES = Path.joinpath(CURRENT_PATH, 'operations.json')
+PATH_WITH_FILES = Path.joinpath(CURRENT_PATH, 'operations.json')
 
 
 def get_data(path):
@@ -10,6 +11,22 @@ def get_data(path):
         return json.load(file)
 
 
-data = get_data(PATH_WITH_FIXTURES)
+data = get_data(PATH_WITH_FILES)
 
-print(data)
+
+def delete_empty_operations(operations):
+    operations.remove({})
+    return operations
+
+
+new_data = delete_empty_operations(data)
+
+
+def first_five_sorted_operations(operations_list):
+    return sorted(operations_list,
+                  key=lambda operation_data: (operation_data['state'], operation_data['date']),
+                  reverse=True
+                  )[:5]
+
+
+five_operations = first_five_sorted_operations(new_data)
